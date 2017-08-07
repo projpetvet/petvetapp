a16f1f54f08bff76c5fac2b54004645e2deb3773
jethro <jethro@kayweb.com.au>
2017-08-07 20:32:52 +0800

    config for test site

--- www/js/config.js
+++ www/js/config.js
@@ -3,7 +3,12 @@
 //     productUrl : 'http://192.168.1.5/webapp/www/images/products/'
 // };
 
+//var config = {
+//    url : 'http://petvet.dev.ph/webservice',
+//    productUrl : 'http://petvet.dev.ph/www/images/products/'
+//};
+
 var config = {
-    url : 'http://petvet.dev.ph/webservice',
-    productUrl : 'http://petvet.dev.ph/www/images/products/'
+    url : 'http://projpetvet.000webhostapp.com/webservice',
+    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/'
 };
