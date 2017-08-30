7394e01d69d48a28f45b989b7fcc23087e2faf0a
jethro <jethro@kayweb.com.au>
2017-08-30 20:07:10 +0800

    login buttons

--- www/js/config.js
+++ www/js/config.js
@@ -1,14 +1,17 @@
 // var config = {
-//     url : 'http://192.168.1.5/webapp/index.php/webservice',
-//     productUrl : 'http://192.168.1.5/webapp/www/images/products/'
+//     url : 'http://192.168.43.50/webapp/index.php/webservice',
+//     productUrl : 'http://192.168.43.50/webapp/www/images/products/',
+//     gmailUrl : 'http://192.168.43.50/webapp/www/signin.jshtml'
 // };
 
-//var config = {
-//    url : 'http://petvet.dev.ph/webservice',
-//    productUrl : 'http://petvet.dev.ph/www/images/products/'
-//};
-
 var config = {
-    url : 'http://projpetvet.000webhostapp.com/webservice',
-    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/'
+    url : 'http://petvet.dev.ph/webservice',
+    productUrl : 'http://petvet.dev.ph/www/images/products/',
+    gmailUrl : 'http://petvet.dev.ph/www/signin.jshtml'
 };
+
+//var config = {
+//    url : 'http://projpetvet.000webhostapp.com/webservice',
+//    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
+//    gmailUrl : 'http://projpetvet.000webhostapp.com/www/signin.jshtml'
+//};
