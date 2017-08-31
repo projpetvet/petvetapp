c2b9bf9993eb3134fb1a8486ad31803be2885d44
jethro <jethro@kayweb.com.au>
2017-09-01 02:53:34 +0800

    live config

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
- var config = {
-     url : 'http://192.168.43.59/webapp/index.php/webservice',
-     productUrl : 'http://192.168.43.59/webapp/www/images/products/',
-     gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
- };
+// var config = {
+//     url : 'http://192.168.43.59/webapp/index.php/webservice',
+//     productUrl : 'http://192.168.43.59/webapp/www/images/products/',
+//     gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
+// };
 
 //var config = {
 //    url : 'http://petvet.dev.ph/webservice',
@@ -10,8 +10,8 @@
 //    gmailUrl : 'http://petvet.dev.ph/www/googlesignin.jshtml'
 //};
 
-//var config = {
-//    url : 'http://projpetvet.000webhostapp.com/webservice',
-//    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
-//    gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
-//};
+var config = {
+    url : 'http://projpetvet.000webhostapp.com/webservice',
+    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
+    gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
+};
