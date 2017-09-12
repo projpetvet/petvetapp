41df8fa8b62b4b5b62860e3338fb69d8b8d06ef4
jethro <jethro@kayweb.com.au>
2017-09-13 03:17:57 +0800

    changes on app config

--- www/js/config.js
+++ www/js/config.js
@@ -1,26 +1,30 @@
-// var config = {
-//     url : 'http://192.168.2.178/webapp/index.php/webservice',
-//     productUrl : 'http://192.168.2.178/webapp/www/images/products/',
-//     gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
-// };
+ var config = {
+     url : 'http://192.168.43.59/webapp/index.php/webservice',
+     productUrl : 'http://192.168.43.59/webapp/www/images/products/',
+     serviceUrl : 'http://192.168.43.59/webapp/www/images/services/',
+     gmailUrl : 'http://projpetvet.000webhostapp.com/www/googlesignin.jshtml'
+ };
 
 //var config = {
 //    url : 'http://petvet.dev.ph/webservice',
 //    productUrl : 'http://petvet.dev.ph/www/images/products/',
+//    serviceUrl : 'http://petvet.dev.ph/www/images/services/',
 //    gmailUrl : 'http://petvet.dev.ph/www/googlesignin.jshtml'
 //};
 
 //var config = {
 //    url : 'http://projpetvet.000webhostapp.com/webservice',
 //    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
+//    serviceUrl : 'http://projpetvet.000webhostapp.com/www/images/services/',
 //    gmailUrl : 'http://projpetvet.000webhostapp.com/signin/google',
 //    fbUrl : 'http://eurobodalla.test.kayweb.com.au/fb.html'
 //};
 
 
-var config = {
-    url : 'http://petvet.dev.ph/webservice',
-    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
-    gmailUrl : 'http://projpetvet.000webhostapp.com/signin/google',
-    fbUrl : 'http://petvet.dev.ph/signin/facebook'
-};
+//var config = {
+//    url : 'http://petvet.dev.ph/webservice',
+//    productUrl : 'http://projpetvet.000webhostapp.com/www/images/products/',
+//    serviceUrl : 'http://projpetvet.000webhostapp.com/www/images/services/',
+//    gmailUrl : 'http://projpetvet.000webhostapp.com/signin/google',
+//    fbUrl : 'http://petvet.dev.ph/signin/facebook'
+//};
